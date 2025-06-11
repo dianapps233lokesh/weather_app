4ca1aa8bc3eae8d84b628bc3477ed81c5e4a2f36
lokesh <lokesh.saini@dianapps.com>
2025-06-11 11:23:07 +0530

    exception handling

--- weather/views.py
+++ weather/views.py
@@ -5,18 +5,24 @@ import requests
 import os
 
 def weather_view(request):
-    if request.method=='POST':
-        location=request.POST.get('location')
-        if location:
-            API_KEY = os.getenv('API_KEY')
-            URL=f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}"
-            resp=requests.get(URL).json()
-            # print(resp)
-            params={}
-            params['temp']=resp['main']['temp']-273
-            params['min_temp']=resp['main']['temp_min']-273
-            params['max_temp']=resp['main']['temp_max']-273
-            params['description']=resp['weather'][0]['description']
-        return render(request,"form.html",{"results":params})
-    else:
-        return render(request,"form.html")
+    try:
+        if request.method=='POST':
+            location=request.POST.get('location')
+            if location:
+                API_KEY = os.getenv('API_KEY')
+                URL=f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={API_KEY}"
+                resp=requests.get(URL).json()
+                
+                print(resp)
+                params={}
+                params['temp']=round(resp['main']['temp']-273,1)
+                params['min_temp']=round(resp['main']['temp_min']-273,1)
+                params['max_temp']=round(resp['main']['temp_max']-273,1)
+                params['description']=resp['weather'][0]['description']
+            return render(request,"form.html",{"results":params})
+        else:
+            return render(request,"form.html")
+    
+    except Exception as e:
+        # err={"msg":e}
+        return render(request,"form.html",{"err":"City not found"})
